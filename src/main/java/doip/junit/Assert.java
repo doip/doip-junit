bdcfcbcba314ec4187ccbdd8c92ac9f573d36423
Marco Wehnert <marco.wehnert@gmx.de>
2019-12-13 19:43:39 +0100

    Initial commit

---
+++ src/main/java/doip/junit/Assert.java
@@ -0,0 +1,34 @@
+package doip.junit;
+
+import doip.logging.LogManager;
+import doip.logging.Logger;
+
+public class Assert {
+
+	private static Logger logger = LogManager.getLogger(Assert.class);
+	
+	public static void assertEquals(String message, String expected, String actual) {
+		try {
+			org.junit.Assert.assertEquals(message, expected, actual);
+		} catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
+	private static String getExceptionAsString(Throwable e) {
+		String s = "";
+		String message = e.getMessage();
+		if (message != null) {
+			s += message;
+		}
+		s += "\n";
+		s += e.getClass().getName() + "\n";
+		StackTraceElement[] elements = e.getStackTrace();
+		for (StackTraceElement element : elements) {
+			s += "    " + element + "\n";
+		}
+		return s;
+		
+	}
+}
