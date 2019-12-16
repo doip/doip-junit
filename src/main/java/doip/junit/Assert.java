8d1b93a26005bb41f7d836a5760fc6cf95e116c2
Marco Wehnert <marco.wehnert@gmx.de>
2019-12-16 14:25:53 +0100

    Added more assert functions

--- src/main/java/doip/junit/Assert.java
+++ src/main/java/doip/junit/Assert.java
@@ -7,9 +7,36 @@ public class Assert {
 
 	private static Logger logger = LogManager.getLogger(Assert.class);
 	
+	public static void assertEquals(String expected, String actual) {
+		try {
+			org.junit.Assert.assertEquals(expected, actual);
+		} catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
 	public static void assertEquals(String message, String expected, String actual) {
 		try {
 			org.junit.Assert.assertEquals(message, expected, actual);
+		} catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
+	public static void asserArrayEquals(byte[] expecteds, byte[] actuals) {
+		try {
+			org.junit.Assert.assertArrayEquals(expecteds, actuals);
+		} catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
+	public static void asserArrayEquals(String message, byte[] expecteds, byte[] actuals) {
+		try {
+			org.junit.Assert.assertArrayEquals(message, expecteds, actuals);
 		} catch (AssertionError e) {
 			logger.error(getExceptionAsString(e));
 			throw e;
