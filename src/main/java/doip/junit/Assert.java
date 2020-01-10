f3d3beeef99af07d5798a0c666ec07870201e9ab
Marco Wehnert <marco.wehnert@gmx.de>
2020-01-10 18:52:19 +0100

    Added a few more assert functions

--- src/main/java/doip/junit/Assert.java
+++ src/main/java/doip/junit/Assert.java
@@ -7,6 +7,24 @@ public class Assert {
 
 	private static Logger logger = LogManager.getLogger(Assert.class);
 	
+	public static void assertTrue(boolean condition) {
+		try {
+			org.junit.Assert.assertTrue(condition);
+		}  catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
+	public static void assertTrue(String message, boolean condition) {
+		try {
+			org.junit.Assert.assertTrue(message, condition);
+		}  catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
 	public static void assertEquals(String expected, String actual) {
 		try {
 			org.junit.Assert.assertEquals(expected, actual);
@@ -37,6 +55,42 @@ public class Assert {
 	public static void assertArrayEquals(String message, byte[] expecteds, byte[] actuals) {
 		try {
 			org.junit.Assert.assertArrayEquals(message, expecteds, actuals);
+		} catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
+	public static void assertEquals(String message, int expected, int actual) {
+		try {
+			org.junit.Assert.assertEquals(message, expected, actual);
+		} catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
+	public static void assertEquals(int expected, int actual) {
+		try {
+			org.junit.Assert.assertEquals(expected, actual);
+		} catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
+	public static void assertNotNull(Object obj) {
+		try {
+			org.junit.Assert.assertNotNull(obj);
+		} catch (AssertionError e) {
+			logger.error(getExceptionAsString(e));
+			throw e;
+		}
+	}
+	
+	public static void assertNotNull(String message, Object obj) {
+		try {
+			org.junit.Assert.assertNotNull(message, obj);
 		} catch (AssertionError e) {
 			logger.error(getExceptionAsString(e));
 			throw e;
